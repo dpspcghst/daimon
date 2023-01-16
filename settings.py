0a94c6b31eb9cff01931c96b3f00531dcaf26c0c
deepspaceghost <deepspaceghost@protonmail.com>
2023-01-16 18:04:54 -0500

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,23 @@
+food = {
+    food1 = { # green
+        "mood": +1, "speed": +24
+    },
+    food2 = { # purple
+        "mood": +1, "flight": +24
+    },
+    food3 = { # red
+        "mood": +1, "strength": +24
+    },
+    food4 = { # yellow
+        "mood": +1, "swimming": +24
+    },
+    food5 = { # bat / black / ghost
+        "intelligence": +20, "flight": +40, "luck": +20, "speed": +8, "strength": +8, "swimming": +8
+    },
+    food6 = { # bear / power / red
+        "intelligence": +20, "flight": -16, "luck": +20, "speed": +4, "strength": +36, "swimming": +8
+    },
+    food7 = { # boar / green / run
+        "intelligence": +20, "flight": -12, "luck": +20, "speed": +32, "strength": +16, "swimming": -4
+    }
+}
