28375f73438673a8762bea956d0548105f3d5911
deepspaceghost <deepspaceghost@protonmail.com>
2023-01-17 14:18:25 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,10 +14,31 @@ food = {
     food5 = { # bat / black / ghost
         "intelligence": +20, "flight": +40, "luck": +20, "speed": +8, "strength": +8, "swimming": +8
     },
-    food6 = { # bear / power / red
+    food6 = { # bear / power / red / roaring
         "intelligence": +20, "flight": -16, "luck": +20, "speed": +4, "strength": +36, "swimming": +8
     },
-    food7 = { # boar / green / run
+    food7 = { # boar / dashing / green / run
         "intelligence": +20, "flight": -12, "luck": +20, "speed": +32, "strength": +16, "swimming": -4
+    },
+    food8 = { # cheetah / face washing / green / run
+        "intelligence": +20, "flight": -8, "luck": +20, "speed": +40, "strength": +8, "swimming": -8
+    },
+    food9 = { # condor / flapping wings / fly / purple
+        "intelligence": +20, "flight": +60, "luck": +20, "speed": -24, "strength": +16, "swimming": +20
+    },
+    food10 = { # deer / drawing / green / run
+        "intelligence": +20, "flight": -12, "luck": +20, "speed": +32, "strength": +16, "swimming": -4
+    },
+    food11 = { # breathing fire / dragon / gold / legendary
+        "intelligence": +20, "flight": +4, "luck": +20, "speed": +8, "strength": +32, "swimming": +20
+    },
+    food12 = { # elephant / performing sit-ups / power / red
+        "intelligence": +20, "flight": -16, "luck": +20, "speed": +20, "strength": +36, "swimming": -8
+    },
+    food13 = { # gorilla / pounding chest / power / red
+        "intelligence": +20, "flight": -12, "luck": +20, "speed": +32, "strength": +16, "swimming": -4
+    },
+    food14 = { # black / ghost / half fish
+        "intelligence": +20, "flight": 0, "luck": +20, "speed": +8, "strength": +24, "swimming": +32
     }
 }
